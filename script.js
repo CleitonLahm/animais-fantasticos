804c25022c67de86d53f3a97cea857c7a9c64f32
CleitonLahm <98617966+CleitonLahm@users.noreply.github.com>
2023-01-25 12:36:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,29 +3,19 @@
 
 
 
-// Mostre no console cada parágrado do site
-
-const p = document.querySelectorAll('p');
-p.forEach((item) => {
-  console.log(item)
-})
-
-// Mostre o texto dos parágrafos no console
-
-// const paragrafo = document.querySelectorAll('p');
 
 
 // Como corrigir os erros abaixo:
-// const imgs = document.querySelectorAll('img');
+const imgs = document.querySelectorAll('img');
 
-// imgs.forEach((item, index) => {
-//   console.log(item, index);
-// });
+imgs.forEach((item, index) => {
+  console.log(item, index);
+});
 
-// let i = 0;
-// imgs.forEach(() => {
-//   console.log(i++)
-// });
+let i = 0;
+imgs.forEach(() => {
+  console.log(i++)
+});
 
-// imgs.forEach(() => i++);
+imgs.forEach(() => i++);
 
