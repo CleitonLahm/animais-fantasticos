30a416f4387568f2228e78ab8332fa8d39162624
CleitonLahm <98617966+CleitonLahm@users.noreply.github.com>
2023-01-25 12:37:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,3 @@
 
 
 
-// Como corrigir os erros abaixo:
-const imgs = document.querySelectorAll('img');
-
-imgs.forEach((item, index) => {
-  console.log(item, index);
-});
-
-let i = 0;
-imgs.forEach(() => {
-  console.log(i++)
-});
-
-imgs.forEach(() => i++);
-
