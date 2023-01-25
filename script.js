c0af29f06cf332265718780c9da1402677a77a64
CleitonLahm <98617966+CleitonLahm@users.noreply.github.com>
2023-01-24 21:01:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,6 @@
 
 
 
-// Retorne no console todas as imagens do site
-
-const imagens = document.querySelectorAll('img');
-console.log(imagens)
-
-// Retorne no console apenas as imagens que começaram com a palavra imagem
-
-const imagem2 = document.querySelector('[imagem]')
-console.log(imagem2)
 
 
-// Selecione todos os links internos (onde o href começa com #)
 
-const links = document.querySelectorAll('[href^="#"]')
-console.log(links)
-
-// Selecione o primeiro h2 dentro de .animais-descricao
-
-const h2 = document.querySelector('.animais-descricao h2');
-console.log(h2)
-
-
-// Selecione o último p do site
-
-const p = document.querySelector('p:last-child');
-console.log(p)
