42534805f137471aa85a5ad56b5379ab8046832a
CleitonLahm <98617966+CleitonLahm@users.noreply.github.com>
2023-01-27 10:18:40 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Adicione a classe ativo a todos os itens do menu
+
+const menuItens = document.querySelectorAll('.menu a');
+
+menuItens.forEach(function(item){
+  item.classList.add('ativo')
+})
+
+
+// Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+
+// Verifique se as imagens possuem o atributo alt
+
+// Modifique o href do link externo no menu
+
