8e5468956b07de1549f9fef9bcb70936c8e26173
CleitonLahm <98617966+CleitonLahm@users.noreply.github.com>
2023-01-27 10:35:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,14 @@ menuItens[0].classList.add('ativo')
 
 // Verifique se as imagens possuem o atributo alt
 
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((img) => {
+  const possuiAtributo = img.hasAttribute('alt')
+  console.log(possuiAtributo)
+  
+})
+
 
 
 // Modifique o href do link externo no menu
