446767e7ad4d6b729b82cf090efd558e060caed2
CleitonLahm <98617966+CleitonLahm@users.noreply.github.com>
2023-01-30 12:31:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,23 @@
 // Verifique a distância da primeira imagem
 // em relação ao topo da página
 
-const imagem1 = document.querySelector('img')
-console.log(imagem1)
+// const imagem1 = document.querySelector('img')
+// console.log(imagem1)
 
-const imagem1offsettop = imagem1.offsetTop;
-console.log(imagem1offsettop);
+// const imagem1offsettop = imagem1.offsetTop;
+// console.log(imagem1offsettop);
 
 // Retorne a soma da largura de todas as imagens
+
+const imagens = document.querySelectorAll('img');
+// console.log(imagens)
+
+imagens.forEach((img) => {
+  const widthimg = img.clientWidth;
+  const totalwidthimgs = widthimg * 6;
+  console.log(totalwidthimgs)
+  
+})
 
 // Verifique se os links da página possuem
 // o mínimo recomendado para telas utilizadas
