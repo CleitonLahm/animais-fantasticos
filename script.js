392be720ef7e55e78f23c86f821fa0a3a1477fa8
CleitonLahm <98617966+CleitonLahm@users.noreply.github.com>
2023-01-30 12:38:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 // Verifique a distância da primeira imagem
 // em relação ao topo da página
 
-// const imagem1 = document.querySelector('img')
-// console.log(imagem1)
+const imagem1 = document.querySelector('img')
+console.log(imagem1)
 
-// const imagem1offsettop = imagem1.offsetTop;
-// console.log(imagem1offsettop);
+const imagem1offsettop = imagem1.offsetTop;
+console.log(imagem1offsettop);
 
 // Retorne a soma da largura de todas as imagens
 
 const imagens = document.querySelectorAll('img');
-// console.log(imagens)
+console.log(imagens)
 
 imagens.forEach((img) => {
   const widthimg = img.clientWidth;
@@ -24,5 +24,21 @@ imagens.forEach((img) => {
 // o mínimo recomendado para telas utilizadas
 // com o dedo. (48px/48px de acordo com o google)
 
+const links = document.querySelectorAll('a');
+
+links.forEach((a) => {
+ const widthA = a.clientWidth;
+ console.log(widthA)
+  if(widthA.clientWidth < 48){
+    console.log('pequena')
+  }else{
+    console.log('omacaco')
+  }
+})
+
 // Se o browser for menor que 720px,
 // adicione a classe menu-mobile ao menu
+
+
+const browserWidth = window.innerWidth;
+console.log(browserWidth)
