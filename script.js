453e35267b9d2e69542405beec339456ad59c821
CleitonLahm <98617966+CleitonLahm@users.noreply.github.com>
2023-02-03 12:20:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -45,7 +45,12 @@ const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]')
 function scrollToSection(event){
   event.preventDefault();
   const href = event.currentTarget.getAttribute('href')
-  const section = document.querySelector('href')
+  const section = document.querySelector(href)
+  
+  section.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  })
 }
 
 linksInternos.forEach((link) => {
