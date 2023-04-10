a2847270c8f2c2159126d92b0bcd3ec84fc4d4f4
CleitonLahm <98617966+CleitonLahm@users.noreply.github.com>
2023-04-10 12:45:57 -0300

    up dropMenu

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@ import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
-import initTolltip from './modules/tooltip.js';
+import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/dropdown-menu.js';
 
 initScrollSuave();
 initAnimacaoScroll();
 initAccordion();
 initTabNav();
 initModal();
-initTolltip();
+initTooltip();
+initDropdownMenu();
