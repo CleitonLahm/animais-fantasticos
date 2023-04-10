4143dedfcb00a28a59be790d9dd72109d03cd6a8
CleitonLahm <98617966+CleitonLahm@users.noreply.github.com>
2023-04-10 08:53:46 -0300

    ii

--- js/modules/tooltip.js
+++ js/modules/tooltip.js
@@ -1,3 +1,23 @@
 export default function initTolltip(){
+
+}
+
+
+const tooltips = document.querySelectorAll('[data-tooltip]');
+
+tooltips.forEach((item) => {
+  item.addEventListener('mouseover', onMouseOver)
+})
+
+function onMouseOver(event) {
+  criarTooltipBox(this);
+  console.log(event)
+}
+
+function criarTooltipBox(element) {
+  const TooltipBox = document.createElement('div')
+  const text = element.getAttribute('aria-label')
+  TooltipBox.classList.add('tooltip')
+  TooltipBox.innerText = text;
   
 }
